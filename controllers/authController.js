87f1ba5d7722250970c9b41fce6d3c93e7af9858
Maicon-Dev <64498803+Maicon-Dev@users.noreply.github.com>
2021-04-06 05:33:55 -0300

    Funcionando com token (12:18)

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const bcrypt = require('bcryptjs')
 
+const jwt = require('jsonwebtoken')
+
+const authConfig = require('../config/auth')
+
 const User = require('../models/user')
 
 const router = express.Router();
+
+function generateToken(params = {}) {
+    return jwt.sign( params, authConfig.secret, {
+        expiresIn: 86400
+    })
+}
 
 router.post('/register', async (req, res) => {
     const { email } = req.body;
@@ -16,7 +26,10 @@ router.post('/register', async (req, res) => {
 
         user.password = undefined
         
-        return res.send({ user })
+        return res.send({ 
+            user,
+            token: generateToken({ id: user.id })
+         })
     } catch (err) {
         return res.status(400).send({ error: 'Registration failed'})
     }
@@ -33,7 +46,12 @@ router.post('/authenticate', async (req, res) => {
     if(!await bcrypt.compare(password, user.password))
         return res.status(400).send({ error: 'Invalid password'})
 
-    res.send({ user })
+    user.password = undefined
+    
+    res.send({ 
+        user, 
+        token: generateToken({ id: user.id })
+    })
 })
 
 module.exports =  app => app.use('/auth', router)
